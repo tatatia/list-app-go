51800b069c280c0e023cf415984f57e957dd56d4
tatatia <thetattianochka@gmail.com>
2021-07-15 11:27:59 +0300

    added Parsing JWT tokens. Middleware authentication

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -3,10 +3,14 @@ package handler
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func (h *Handler) createList(c *gin.Context) {
-
+	id, _ := c.Get(userCtx)
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 func (h *Handler) getAllLists(c *gin.Context) {
 	fmt.Println("hello")
